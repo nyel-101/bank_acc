d9010192d7985121215d30cc018ea9a5ba7f1f3a
nyel-101 <dandevjr@gmail.com>
2025-02-19 18:40:17 +0000

    feat(api): sanitize auth tokens

--- fury/styles/queries.py
+++ fury/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_96fdc5ea005a4432ac9dd4daa1fac3ab = () => {
-  return '2026-02-28T21:59:18Z';
+const node_4f27840a7b8c4056930a8b26e31c463a = () => {
+  return '2025-02-19T18:40:17Z';
 };
