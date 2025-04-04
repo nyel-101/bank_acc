1e25b8d2b816e225c59dd6f3f56d7f08ddf7623b
nyel-101 <dandevjr@gmail.com>
2025-04-04 10:16:32 +0000

    style(db): restructure auth tokens

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c611ea248e5645368fd539f43b70317d = () => {
-  return '2026-05-25T04:18:47Z';
+const node_06289a3c325445608fe18a331c450295 = () => {
+  return '2025-04-04T10:16:32Z';
 };
