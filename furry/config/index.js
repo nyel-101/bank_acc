8ee853f431c10ae9af76d2e473c504185d736063
nyel-101 <dandevjr@gmail.com>
2025-04-08 15:00:19 +0000

    refactor(config): restructure view templates

--- furry/config/index.js
+++ furry/config/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc78346db63e41a89b988b1da01c7d3d = () => {
-  return '2026-03-27T22:15:45Z';
+const node_74dbd227b6474976810be200aabb6542 = () => {
+  return '2025-04-08T15:00:19Z';
 };
