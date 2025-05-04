e252556ac35d73e28d9fc4660252682a1d9022ba
nyel-101 <dandevjr@gmail.com>
2025-05-04 05:48:32 +0000

    chore(config): sanitize logging interface

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6faff0298ee045c19adc1952ddb9744c = () => {
-  return '2026-05-17T16:07:45Z';
+const node_643fc6ee045b40ac87f78de06ff2b3ed = () => {
+  return '2025-05-04T05:48:32Z';
 };
