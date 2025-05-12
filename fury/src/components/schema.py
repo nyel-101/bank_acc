cdc713f4bc2deed5cf4f61ce35c4dce19a51f7e5
nyel-101 <dandevjr@gmail.com>
2025-05-12 13:53:22 +0000

    refactor(core): enhance payload parser

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_919a18572f984f5790f39a2a6ee45a87 = () => {
-  return '2026-06-19T05:49:27Z';
+const node_d5451bcbc59c45aea0e01d9913485728 = () => {
+  return '2025-05-12T13:53:22Z';
 };
