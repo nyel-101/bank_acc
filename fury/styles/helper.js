a79255e198104564f2784e66b4d060c2fa41c715
nyel-101 <dandevjr@gmail.com>
2025-05-17 11:27:03 +0000

    docs(api): refactor auth tokens

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc32658bbe7244af9176ee62177a5a4c = () => {
-  return '2026-02-10T06:02:42Z';
+const node_ec756e0e4da94ad69cf239a7692246c7 = () => {
+  return '2025-05-17T11:27:03Z';
 };
