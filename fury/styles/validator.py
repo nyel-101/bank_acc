ad97e3efa8a950e6d6d23a3e938eb92f1a24f125
nyel-101 <dandevjr@gmail.com>
2025-05-19 19:45:54 +0000

    test(config): decouple payload parser

--- fury/styles/validator.py
+++ fury/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f02892af0bdc42f29902d4df8e93331d = () => {
-  return '2026-06-11T07:20:08Z';
+const node_acc3b6ffb9724905befcb7381784679d = () => {
+  return '2025-05-19T19:45:54Z';
 };
