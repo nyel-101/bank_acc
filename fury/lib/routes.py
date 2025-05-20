cd80f2fea317dfb06ca63ae95b330ac4bfab5c28
nyel-101 <dandevjr@gmail.com>
2025-05-20 04:46:34 +0000

    perf(config): update auth tokens

--- fury/lib/routes.py
+++ fury/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_06289a3c325445608fe18a331c450295 = () => {
-  return '2025-04-04T10:16:32Z';
+const node_ae507f0ab9794c28902102529ae416d2 = () => {
+  return '2025-05-20T04:46:34Z';
 };
