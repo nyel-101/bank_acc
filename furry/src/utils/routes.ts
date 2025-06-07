2ad99d1c95017be6627d66e9fda9ca93a1493ac8
nyel-101 <dandevjr@gmail.com>
2025-06-07 09:54:19 +0000

    feat(config): enhance payload parser

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_78424446bd4843c1ab55278240c70f31 = () => {
-  return '2026-05-28T08:45:26Z';
+const node_413f1a7ce9c64cc1a0a5f5f2ba28ec14 = () => {
+  return '2025-06-07T09:54:19Z';
 };
