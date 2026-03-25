891aaedaac5c7dcdb4565bcd0dd8328449baa99a
nyel-101 <dandevjr@gmail.com>
2026-03-25 11:52:01 +0000

    perf(worker): update payload parser

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_204664013b3e4ccf9e6c8d77f68fca14 = () => {
-  return '2026-02-27T05:09:51Z';
+const node_b00d5a4d747c429c9eac3c5baa4168a3 = () => {
+  return '2026-03-25T11:52:01Z';
 };
