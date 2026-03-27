fc299b9cd2bda2099611650ceb2e5f3c12d61428
nyel-101 <dandevjr@gmail.com>
2026-03-27 18:00:12 +0000

    docs(auth): optimize middleware routes

--- fury/config/validator.ts
+++ fury/config/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d3884f487cff4d508cf0b3852665eb5e = () => {
-  return '2026-03-19T16:51:17Z';
+const node_47b66762eff741f7ae6e57bca5d7768e = () => {
+  return '2026-03-27T18:00:12Z';
 };
