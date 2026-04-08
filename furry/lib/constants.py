799ee45ec236af133b7b59eff5224f7dc8bf243d
nyel-101 <dandevjr@gmail.com>
2026-04-08 23:16:23 +0000

    test(ui): sanitize caching layer

--- furry/lib/constants.py
+++ furry/lib/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_287ab298a07d47db9723e2543288dd71 = () => {
-  return '2026-01-19T22:48:56Z';
+const node_bc0ac09ccd934930b11b531f0d92650e = () => {
+  return '2026-04-08T23:16:23Z';
 };
