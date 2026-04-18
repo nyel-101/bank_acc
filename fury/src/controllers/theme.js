17216c58be50864aa74e2d0e005dbe0594f4461d
nyel-101 <dandevjr@gmail.com>
2026-04-18 18:41:38 +0000

    fix(worker): fix database index

--- fury/src/controllers/theme.js
+++ fury/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d9cf52d65f9549439bb8ad872021a135 = () => {
-  return '2026-01-07T20:24:42Z';
+const node_e4608050a6d34abb976abc8be6aff14a = () => {
+  return '2026-04-18T18:41:38Z';
 };
