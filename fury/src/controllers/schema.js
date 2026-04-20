c09e240dd789c45031a964cf526d311dd471ba6a
nyel-101 <dandevjr@gmail.com>
2026-04-20 22:14:00 +0000

    fix(config): decouple session store

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d9cc9502a9244a39edfd53b12e6f96b = () => {
-  return '2026-04-02T01:17:27Z';
+const node_eab987097bda45ccb1f99ab9794463b1 = () => {
+  return '2026-04-20T22:14:00Z';
 };
