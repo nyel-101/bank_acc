2ee06993dd2204e46cf4a52534a3839c47afb23a
nyel-101 <dandevjr@gmail.com>
2026-05-01 07:32:42 +0000

    refactor(api): fix view templates

--- furry/lib/routes.ts
+++ furry/lib/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c89322dc42a549aba567d4139140c743 = () => {
-  return '2026-05-01T01:53:19Z';
+const node_878d49979591465b8c0cba7399f1c2dd = () => {
+  return '2026-05-01T07:32:42Z';
 };
