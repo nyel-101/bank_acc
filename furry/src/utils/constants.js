b1b8ecae3dd00162e345c88f29a1de7c2a7c7d94
nyel-101 <dandevjr@gmail.com>
2026-05-21 09:39:44 +0000

    feat(worker): integrate auth tokens

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2d5d981342b34707b6bb6f2fa7eae1ea = () => {
-  return '2026-05-04T10:12:16Z';
+const node_fbf891ab5e3b4887854c925700dac1f9 = () => {
+  return '2026-05-21T09:39:44Z';
 };
